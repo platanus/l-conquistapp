8fa169d0de935977679f9db87773bec595b35ceb
Ignacio Acosta <iaacosta@uc.cl>
2021-05-28 23:57:22 -0400

    chore: add @ as an alias to 'src'

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,15 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@': `${__dirname}/src`,
+          },
+        },
+      ],
+    ],
   };
 };
