8b707b2cf5acfee15bd476a1bfef498b156c2deb
Alejandra Rojas <alejandra@platan.us>
2021-06-04 18:43:19 -0400

    feat(home): use open gate mutation

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from 'react-query';
 import Home from './src/Home';
 
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <Home />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
 }
