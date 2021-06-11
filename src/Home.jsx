b4bf4a207557e3ae1feae49325c5d9831239b657
Ignacio Acosta <iaacosta@uc.cl>
2021-06-11 16:56:11 -0400

    feat(home): add loader

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import useOpenGate from '@/api/gate';
-import { Text, TouchableOpacity, View } from 'react-native';
-import tailwind from './utils/tailwind';
+import { Text, TouchableOpacity, View, ActivityIndicator } from 'react-native';
+import tailwind, { getColor } from './utils/tailwind';
 
 export default function Home() {
-  const { mutate } = useOpenGate();
+  const { mutate, isLoading, isError } = useOpenGate();
 
   return (
     <View style={tailwind('bg-pl-purple flex-1 justify-center')}>
@@ -13,12 +13,19 @@ export default function Home() {
           L&lsquo; ConquistApp
         </Text>
         <TouchableOpacity
-          style={tailwind('bg-pl-yellow p-2 rounded-lg flex-row justify-between')}
+          style={tailwind('bg-pl-yellow p-2 rounded-lg flex-row justify-center')}
           onPress={() => mutate()}
         >
-          <Text style={tailwind('text-pl-purple text-lg text-center w-full')}>
-            ¡Abrir reja ahora ya!
+          <Text style={tailwind('text-pl-purple text-lg')}>
+            ¡Abrir la reja ahora ya!
           </Text>
+          {isLoading && (
+            <ActivityIndicator
+              size="small"
+              color={getColor('pl-purple')}
+              style={tailwind('ml-2')}
+            />
+          )}
         </TouchableOpacity>
 
       </View>
